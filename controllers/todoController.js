3145fda0962263e1dce34afbfbd85b3344a35247
githubscientist <sathish.datascientist@gmail.com>
2026-07-09 18:49:19 +0530

    connected to mongodb database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,46 +1,15 @@
-const todos = [
-    {
-        "createdAt": "2026-06-18T22:52:46.591Z",
-        "title": "Buy Groceries",
-        "description": "Buy the following groceries\n- milk\n- vegetables",
-        "isDone": false,
-        "id": "7"
-    },
-    {
-        "createdAt": "2026-06-19T01:58:20.928Z",
-        "title": "Attend the New Site Config Meeting",
-        "description": "Meeting today at 8.30PM IST",
-        "isDone": true,
-        "id": "8"
-    },
-    {
-        "createdAt": "2026-06-19T04:17:59.186Z",
-        "title": "Amount Transfer to Auditor",
-        "description": "Do an amount transfer for GST taxes to Auditor",
-        "isDone": false,
-        "id": "9"
-    },
-    {
-        "createdAt": "2026-07-07T02:51:02.219Z",
-        "title": "Complete Task #543 before tomorrow 5pm",
-        "description": "Task #543 has 3 features which needs to be completed as a quick development fix post production",
-        "isDone": false,
-        "id": "10"
-    }
-];
-
 const todoController = {
     getAllTodos: (request, response) => {
-        response.status(200).json(todos);
+        response.status(200).json({ message: 'GET all todos' });
     },
     createTodo: (request, response) => {
-        response.send('POST todo');
+        response.status(200).json({ message: 'Create a new todo' });
     },
     updateTodo: (request, response) => {
-        response.send('PUT todo');
+        response.status(200).json({ message: 'Update todo' });
     },
     deleteTodo: (request, response) => {
-        response.send('DELETE todo');
+        response.status(200).json({ message: 'Delete todo' });
     }
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-// 3. start the server to listen for requests
-app
-    .listen(3001, 'localhost', () => {
-        console.log(`server is running at http://localhost:3001`);
-    });
+// this will allow us to use the variables
+// in .env file here in this server.js
+dotenv.config();
+
+// connect to the mongodb database
+mongoose    
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB database...');
+
+        // start the server to listen for requests
+        app
+            .listen(3001, 'localhost', () => {
+                console.log(`Server is running at http://localhost:3001...`);
+            });
+    })
+    .catch((error) => {
+        console.log('Error in connecting to the database');
+        console.log(`Error:`, error.message);
+    })
