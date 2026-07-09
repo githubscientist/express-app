6725d2ac1b4ded3831c799bb7bffedb94c2cadbd
githubscientist <sathish.datascientist@gmail.com>
2026-07-09 18:58:04 +0530

    config --to add a layer of abstraction with constants

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const app = require("./app");
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-// this will allow us to use the variables
-// in .env file here in this server.js
-dotenv.config();
+const { MONGODB_URI, PORT, HOST } = require("./utils/config");
 
 // connect to the mongodb database
 mongoose    
-    .connect(process.env.MONGODB_URI)
+    .connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB database...');
 
         // start the server to listen for requests
         app
-            .listen(3001, 'localhost', () => {
-                console.log(`Server is running at http://localhost:3001...`);
+            .listen(PORT, HOST, () => {
+                console.log(`Server is running at http://${HOST}:${PORT}...`);
             });
     })
     .catch((error) => {
